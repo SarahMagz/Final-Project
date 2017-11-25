fd84d5744bf2c3da8ec7baae4d4755ede0afe93e
Tara Jørgensen <traarj@gmail.com>
2017-11-25 19:23:45 +0100

    Omstrukturerte kode

--- functions.js
+++ functions.js
@@ -1,18 +1,18 @@
 function countdown() {
     var departure = new Date("Jan 1, 2020 00:00:00").getTime(); // Reiser ved midnatt, 1. januar
 
-    var x = setInterval(function() {
+    var x = setInterval(function () {
         var toDay = new Date().getTime(); //Todays date
         var timeLeft = departure - toDay; // Time left
 
-        var years = Math.floor (timeLeft / (1000 * 60 * 60 * 24 * 365));
-        var days = Math.floor ((timeLeft % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
-        var hours = Math.floor ((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor ((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor ((timeLeft % (1000 * 60)) / 1000);
+        var years = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 365));
+        var days = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
         document.getElementById("countdown").innerHTML = "Departure in " + years +
-        "y " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            "y " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
         if (timeLeft < 0) {
             clearInterval(x);
@@ -21,8 +21,8 @@ function countdown() {
     }, 1000);
 }
 
-function smoothScroll () {
-    document.querySelector('#video').scrollIntoView({ 
+function smoothScroll() {
+    document.querySelector('#video').scrollIntoView({
         behavior: 'smooth'
     })
 }
